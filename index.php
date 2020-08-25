378d2bb7816b41905860c4d0ddb1992953959a7f
Soufiane El kabir <elkabir.soufiane.pro@gmail.com>
2020-08-25 16:33:10 +0200

    premier formulaire

--- index.php
+++ index.php
@@ -1,1 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="assetsCSS/style.css">
+    <title>Create an account</title>
+  </head>
+  <body>
+  <h1 class="titre">BlurFlix</h1>
+  <section class="container">
+    <section class="row justify-content-center">
+        <section class="col-12 col-sm-6 col-md-3">
+        <form class="form-container">
+            <div class="form-group">
+                <label for="formGroupExampleInput">Username</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nickname">
+            </div>
+            <div class="form-group">
+                <label for="formGroupExampleInput2">password</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="password1234">
+            </div>
+            <button type="login" class="btn btn-primary btn-block">login</button>
+            </br>
+            <p>First time on BlurFlix? <a href="">register</a> </p>
+            </br>
+            <p>Login problem? <a href="">Click here</a> </p>
+        </form>
+        </section>      
+    </section>
+
+  </section>
+<!-- FOOTER -->
+<footer class="bg-secondary">
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <!-- <h1 class="text-white text-capitalize font-weight-light p-3">BlurFlix</h1> -->
+                <h3 class="text-black font-weight-light mb-3">For video quality, we only have a blur name</h3>
+                <p>&copy;Copyright 2020 - by Becode</p>
+            </div>
+
+        </div>
+
+    </div>
+</footer>
+  
+<!-- END FOOTER -->
+  
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
