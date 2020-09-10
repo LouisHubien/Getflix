6a27217f084dd5f312884c1e9737a466819a078a
unknown <noemie.uylenbroeck@gmail.com>
2020-09-10 14:21:16 +0200

    ajout

--- admin.php
+++ admin.php
@@ -34,8 +34,65 @@ include 'sessionstart.php';
         <div class='row mt-5'>
             <div class="col-8 offset-1">
                 <h2> Add a video</h2>
-                <form>
-
+                <!-- Message de confirmation-->
+                <p class="text-white">
+                <?php
+                include 'connectiondatabase.php';
+                if(isset($_SESSION['confirmAdd'])){
+                    print_r($_SESSION['confirmAdd'][0]);
+                    unset($_SESSION['confirmAdd']);
+                }
+                ?>
+                </p> 
+                <form class="form-container" action="admincible.php" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" class="form-control" id="title" placeholder="Title" name="title" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="thumbnailname">Thumbnail's name</label>
+                        <input type="text" class="form-control" id="thumbnailname" placeholder="Thumbnail's name" name="thumbnailname" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="thumbnail">Thumbnail (.JPG 200x300px = thumbnail's name.jpg)</label>
+                        <input type="file" class="form-control" id="thumbnail" name="thumbnail" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="preview">Youtube link</label>
+                        <input type="text" class="form-control" id="preview" placeholder="link" name="preview" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="category">Choose a genre</label>
+                        <select name="category" class="form-control" id="category">
+                            <option value="1">Action/Aventure</option>
+                            <option value="2">Comedy</option>
+                            <option value="3">Drama</option>
+                            <option value="4">Horror</option>
+                            <option value="5">Romantic</option>
+                            <option value="6">Sci-Fi & Fantasy</option>
+                            <option value="7">Thrillers</option>
+                        </select>
+                    </div>
+                    <p>Is this a movie or a TV-show? <p>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" id="movie"  name="movies" value="1" required>
+                        <label class="form-check-label" for="movie">Movie</label>
+                    </div>
+                    <div class="form-group form-check form-check-inline">
+                        <input class="form-check-input" type="radio"  id="tvshow"  name="movies" value="0">
+                        <label class="form-check-label" for="tvshow">TV-show</label>
+                    </div>
+                    <p>Is this a movie for kids ? <p>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" id="adult"  name="adult" value="1" required>
+                        <label class="form-check-label" for="adult">No</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio"  id="kid"  name="adult" value="0">
+                        <label class="form-check-label" for="kid">Yes</label>
+                    </div>
+                    </br>
+                    <input type="submit" value="Add" class="btn buttonconfirm text-white">
                 </form>
             </div>
         </div>
--- admincible.php
+++ admincible.php
@@ -1,5 +1,29 @@
 <?php
 include 'connectiondatabase.php';
+//Upload l'image
+if (isset($_FILES['thumbnail']) AND $_FILES['thumbnail']['error'] == 0)
+{
+    $infosfichier = pathinfo($_FILES['thumbnail']['name']);
+    $extension_upload = $infosfichier['extension'];
+    if($extension_upload='jpg'){
+        move_uploaded_file($_FILES['thumbnail']['tmp_name'], 'thumbnail/' . basename($_FILES['thumbnail']['name']));
+    }
+};
+
+//Ajoute le film dans la database
+if(isset($_POST['title'],$_POST['thumbnailname'],$_POST['preview'],$_POST['category'],$_POST['movies'],$_POST['adult'])){
+    $title=htmlspecialchars($_POST['title']);
+    $thumbnailname="<img src=\"thumbnail" . htmlspecialchars($_POST['thumbnailname']) . ".jpg\" alt=\"" . htmlspecialchars($_POST['thumbnailname']) . "\">";
+    $preview="<iframe width=\"715\" height=\"402\" src=\"" . htmlspecialchars($_POST['preview']) . "\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
+    $category=htmlspecialchars($_POST['category']);
+    $movies=htmlspecialchars($_POST['movies']);
+    $adult=htmlspecialchars($_POST['adult']);
+    
+    $bdd->exec('INSERT INTO entities(Name, Thumbnails, Preview, CategoryId, movies, adult) 
+    VALUES($title, $thumbnailname, $preview, $category, $movies, $adult)');
+    $_SESSION['confirmAdd']=array("The video has been added to the database.");
+    header('Location:admin.php');
+}
 
 //Efface une entities
 if(isset($_GET['titre'])){
